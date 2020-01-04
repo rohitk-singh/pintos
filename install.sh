adcc24d08d2826c24d583c15e74630e2df4eb211
Trey Frank Boehm <trey.frank.boehm@gmail.com>
2020-01-04 13:06:31 -0600

    Friendlier installation script
     * No more `.PINTOS_PATH` file. Use an environment variable instead.
     * Users must modify their bashrc to augment the path.
     * The script does as much as it can, and exits with 1 if anything
       fails. In particular, it will try to make utils and symlink qemu,
       even if the first one fails.

--- install.sh
+++ install.sh
@@ -1,83 +1,66 @@
+#!/bin/bash
+
 SCRIPT=`realpath -s $BASH_SOURCE[0]`
 SCRIPTPATH=`dirname $SCRIPT`
 
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+NC="\033[0m"
+
 run () {
-  ./.check_reqs.sh
-  if [[ $? != '0' ]]; then
-    echo 'FATAL: Requirements not met, please follow recommended installation'
-    echo 'commands before continuing.'
-    return 1
-  fi
+    STATUS=0
 
-  # Install pintos
-  which pintos &> /dev/null
-  if [[ $? != '0' ]]; then
-    echo 'Installing pintos to PATH ...'
-
-    echo "export PATH=\$PATH:$SCRIPTPATH/utils" > .PINTOS_PATH
-
-    # No bashrc yet? Just make a blank
-    touch ~/.bashrc
-    # Backup bashrc
-    cp ~/.bashrc ~/.bashrc.PINTOS.BAK
-    echo >> ~/.bashrc
-    echo "# Put pintos in PATH" >> ~/.bashrc
-    echo "source $SCRIPTPATH/.PINTOS_PATH" >> ~/.bashrc
-
-    # alias pintos-gdb while we're at it
-    MACROS_PATH=$SCRIPTPATH/misc/gdb-macros
-    echo "alias pintos-gdb='GDBMACROS=$MACROS_PATH pintos-gdb'" >> ~/.bashrc
-
-    source ~/.bashrc
-    if [[ $? != '0' ]]; then
-      echo "Installing pintos to PATH failed!"
-      cp -f ~/.bashrc.PINTOS.BAK ~/.bashrc
-      return 1
+    ./.check_reqs.sh
+    if [[ $? != 0 ]]; then
+        echo 'FATAL: Requirements not met, please follow recommended installation'
+        echo 'commands before continuing.'
+        return 1
     fi
 
-    source .PINTOS_PATH
-    which pintos &> /dev/null
-    if [[ $? != '0' ]]; then
-      echo "Installing pintos to PATH failed!"
-      cp -f ~/.bashrc.PINTOS.BAK ~/.bashrc
-      return 1
+    # Do *not* write to the bashrc directly. This can create problems when
+    # people try to reinstall Pintos.
+    printf "${YELLOW}Add the following two lines to the end of your ~/.bashrc:${NC}\n"
+    printf "export PINTOS=$SCRIPTPATH\n"
+    printf "export PATH=\$PATH:\$PINTOS/utils\n\n"
+
+    printf "${YELLOW}Run the following command:${NC}\n"
+    printf "source ~/.bashrc\n\n"
+
+    # Build pintos utils
+    printf  "Making Pintos utils ... "
+    pushd utils &> /dev/null
+    make > /dev/null
+    if [[ $? != 0 ]]; then
+        printf "${RED}failed!${NC}\n"
+        STATUS=1
+    else
+        printf "${GREEN}done.${NC}\n"
     fi
-
-    echo "DONE"
-    echo
-  fi
-
-  # Build pintos utils
-  echo "Making pintos utils ..." 
-
-  pushd utils &> /dev/null
-  make &> /dev/null
-  if [[ $? != '0' ]]; then
-    echo "Building pintos utils failed!"
     popd &> /dev/null
-    return 1
-  fi
-  popd &> /dev/null
 
-  echo "DONE"
-  echo
-
-  # Install QEMU
-  which qemu &> /dev/null
-  if [[ $? != '0' ]]; then
-    echo 'Symlinking qemu-system-i386 -> qemu ...'
-
-    QEMU_SYSTEM=`which qemu-system-i386`
-    QEMU_PATH=`dirname $QEMU_SYSTEM`
-    ln -s $QEMU_SYSTEM utils/qemu 
-
+    # Install QEMU
     which qemu &> /dev/null
-    if [[ $? != '0' ]]; then
-      echo "Symlinking qemu failed!"
-      return 1
+    if [[ $? != 0 ]]; then
+        printf 'Symlinking qemu-system-i386 -> qemu ... '
+        QEMU_SYSTEM=`which qemu-system-i386`
+        QEMU_PATH=`dirname $QEMU_SYSTEM`
+        ln -s $QEMU_SYSTEM utils/qemu &> /dev/null
+
+        which qemu &> /dev/null
+        if [[ $? != 0 ]]; then
+            printf "${RED}failed!${NC}\n"
+            STATUS=1
+        else
+            printf "${GREEN}done.${NC}\n"
+        fi
     fi
-    echo "DONE"
-  fi
+
+    if [[ $STATUS != 0 ]]; then
+        printf "${RED}Installation unsuccessful.${NC}\n"
+    fi
+
+    return $STATUS
 }
 
 run
