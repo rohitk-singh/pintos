ee72ed926c9af553c4c8b34bff7f3d998df15593
Trey Frank Boehm <trey.frank.boehm@gmail.com>
2020-01-25 17:01:30 -0600

    Uncrustify userprog subdirectory

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,21 +1,23 @@
-#include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "userprog/syscall.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *);
 
 void
-syscall_init (void)
+syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+    intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED)
+syscall_handler(struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
+    /* Remove these when implementing syscalls */
+    printf("system call!\n");
 
-  thread_exit ();
+    thread_exit();
 }
